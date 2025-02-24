077dbac5d8f81e86f5504b17466289e4de2d21f1
if-loop69420 <j.sztavi@pm.me>
2025-02-24 13:34:34 +0100

    init: add all this stuff to a repository

---
+++ distcc_image/start.sh
@@ -0,0 +1,4 @@
+docker run -p 3633:3632 -d distcc_docker
+docker run -p 3634:3632 -d distcc_docker
+docker run -p 3635:3632 -d distcc_docker
+docker run -p 3636:3632 -d distcc_docker
---
+++ nginx_image/start.sh
@@ -0,0 +1,2 @@
+docker run -v ./nginx.conf:/etc/nginx/nginx.conf:ro -p 3632:3632 --network=host nginx
+
